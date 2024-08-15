8f2edfc472d3883043f150f92c0278bc2ea71f9d
pratikhande101 <pratikhande30@gmail.com>
2024-08-15 11:11:31 +0530

    Initial commit with User entity

---
+++ src/main/java/com/pratik/cash_rich_assignment/assignment/model/User.java
@@ -0,0 +1,42 @@
+package com.pratik.cash_rich_assignment.assignment.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String mobile;
+}
